507376cad4915f27cc306adab0235ab9cdd93840
Romain LEMBO <romainlembo06@gmail.com>
2019-12-23 19:29:08 +0100

    #6 - some tests ok

--- src/test/java/com/ecommerce/microcommerce/web/controller/ProductControllerTest.java
+++ src/test/java/com/ecommerce/microcommerce/web/controller/ProductControllerTest.java
@@ -1,14 +1,14 @@
 package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.model.Product;
-import com.ecommerce.microcommerce.web.exceptions.ProduitInternalErrorException;
-import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -47,6 +47,45 @@ public class ProductControllerTest {
         Product bodyRequestCreated = new Product(5, "Produit", 10, 10);
         ResponseEntity requestCreated = this.restTemplate.postForEntity(baseUrl + port + "/Produits", bodyRequestCreated, String.class);
         assertThat(requestCreated.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        Product bodyRequestFree = new Product(5, "Produit", 0, 0);
+        ResponseEntity requestFree = this.restTemplate.postForEntity(baseUrl + port + "/Produits", bodyRequestFree, String.class);
+        assertThat(requestFree.getStatusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @Test
+    public void supprimerProduit() throws Exception {
+        ResponseEntity requestOK = this.restTemplate.exchange(baseUrl + port + "/Produits/1", HttpMethod.DELETE, HttpEntity.EMPTY, String.class);
+        assertThat(requestOK.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        ResponseEntity requestFailed = this.restTemplate.exchange(baseUrl + port + "/Produits/4", HttpMethod.DELETE, HttpEntity.EMPTY, String.class);
+        assertThat(requestFailed.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void updateProduit() throws Exception {
+        Product bodyRequestOk = new Product(1, "Produit", 10, 10);
+        ResponseEntity requestOK = this.restTemplate.exchange(baseUrl + port + "/Produits/", HttpMethod.PUT, new HttpEntity<>(bodyRequestOk), String.class);
+        assertThat(requestOK.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void testeDeRequetes() throws Exception {
+        ResponseEntity requestOK = this.restTemplate.getForEntity(baseUrl + port + "/test/produits/100", String.class);
+        assertThat(requestOK.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        ResponseEntity requestNotFound= this.restTemplate.getForEntity(baseUrl + port + "/test/produits/1000", String.class);
+        assertThat(requestNotFound.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void calculerMargeProduits() throws Exception {
+        /* TODO : Faire test avec auth OK (pour Gabriel ?)
+        ResponseEntity requestOK = this.restTemplate.getForEntity(baseUrl + port + "/AdminProduits", String.class);
+        assertThat(requestOK.getStatusCode()).isEqualTo(HttpStatus.OK);*/
+
+        ResponseEntity requestUnAuth = this.restTemplate.getForEntity(baseUrl + port + "/AdminProduits", String.class);
+        assertThat(requestUnAuth.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
     @Test
